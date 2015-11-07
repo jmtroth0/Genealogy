6ead79581b6bd6cf6e00e75e3876eae53009c3e8
Jordan Roth <jmtroth0@gmail.com>
2015-11-07 16:58:59 -0500

    Add courtesy methods to get immediate family of a family member

--- app/assets/javascripts/models/family_member.js
+++ app/assets/javascripts/models/family_member.js
@@ -3,5 +3,65 @@ Genealogy.Models.FamilyMember = Backbone.Model.extend({
 
   name: function () {
     return this.escape('fname') + " " + this.escape('lname');
-  }
+  },
+
+  parentA: function () {
+    return this.collection.getOrFetch(this.get('parent_a_id'));
+  },
+
+  parentB: function () {
+    return this.collection.getOrFetch(this.get('parent_b_id'));
+  },
+
+  parents: function () {
+    var parents = [];
+    if (this.get('parent_a_id')) parents.push(this.parentA());
+    if (this.get('parent_b_id')) parents.push(this.parentB());
+    return parents;
+  },
+
+  children: function () {
+    return this.collection.filter(function (person){
+      return person.get('parent_a_id') === this.id ||
+             person.get('parent_b_id') === this.id;
+    }.bind(this));
+  },
+
+  // getSortedFamily: function () {
+  //   // var descendants = this.getDescendants();
+  //   var ancestors = this.getAncestors();
+  //   return ancestors.concat(descendants.reverse());
+  // },
+  //
+  // getDescendants: function () {
+  //   var children = this.children();
+  //   if (children.length === 0) return [[this]];
+  //   var descendants = [];
+  //   children.forEach(function (child) {
+  //     var subDescendants = child.getDescendants();
+  //     this.addToFamilySet(descendants, subDescendants);
+  //   }.bind(this));
+  //   return descendants;
+  // },
+  //
+  // getAncestors: function () {
+  //   var parents = this.parents();
+  //   var ancestors = [];
+  //   parents.forEach(function (parent) {
+  //     var subAncestors = parent.getAncestors();
+  //     this.addToFamilySet(ancestors, subAncestors);
+  //   }.bind(this));
+  //   return ancestors;
+  // },
+  //
+  // addToFamilySet: function(set, subset){
+  //   subset.forEach(function(generation, idx){
+  //     if (set[set.length - idx]) {
+  //       set[set.length - idx].push(generation);
+  //     } else {
+  //       // set += generation;
+  //       set.push(generation);
+  //     }
+  //   });
+  // }
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create!({username: 'mbars', password: 'password', fname: 'Oranos', lname: 'Marx'})
+oranos = user.family_members.first
+gaia = user.family_members.create!({fname: 'Gaia', lname: 'Hayek'})
+rhea = user.family_members.create!({
+  fname: 'Rhea', lname: 'Hayek', parent_a: oranos, parent_b: gaia
+})
+kronos = user.family_members.create!({
+  fname: 'Kronos', lname: 'Marx', parent_a: oranos, parent_b: gaia
+})
+hyperion = user.family_members.create!({
+  fname: 'Hyperion', lname: 'Marx', parent_a: oranos, parent_b: gaia
+})
+zeus = user.family_members.create!({
+  fname: 'Zeus', lname: 'Marx', parent_a: kronos, parent_b: rhea
+})
+hera = user.family_members.create!({
+  fname: 'Hera', lname: 'Hayek', parent_a: kronos, parent_b: rhea
+})
+athena = user.family_members.create!({
+  fname: 'Athena', lname: 'Hayek', parent_a: zeus
+})
+ares = user.family_members.create!({
+  fname: 'Ares', lname: 'Marx', parent_a: zeus, parent_b: hera
+})
