6826142af40e021690ab01c437a2d763cea79a21
Jordan Roth <jmtroth0@gmail.com>
2015-11-11 17:16:33 -0500

    Make main page responsive to resize events

--- app/assets/javascripts/views/users/user_main.js
+++ app/assets/javascripts/views/users/user_main.js
@@ -5,10 +5,20 @@ Genealogy.Views.UserMain = Backbone.View.extend({
 
   initialize: function (options) {
     this.listenTo(this.model, 'sync', this.render);
+    $(window).on("resize", this.setNavLocation.bind(this));
   },
 
   render: function () {
     this.$el.html(this.template({user: this.model}));
+    this.setNavLocation();
     return this;
+  },
+
+  setNavLocation: function (e) {
+    if (window.innerWidth > 600) {
+      this.$el.find('ul.nav-options').css('width', '480px');
+    } else {
+      this.$el.find('ul.nav-options').css('width', '160px');
+    }
   }
 });
