60afd421df11eb7ae25d5a1e8f72355a8860dc76
Jordan Roth <jmtroth0@gmail.com>
2015-11-12 19:32:14 -0500

    Style photo show modal

--- app/assets/javascripts/views/photos/show.js
+++ app/assets/javascripts/views/photos/show.js
@@ -5,16 +5,32 @@ Genealogy.Views.ShowPhoto = Backbone.View.extend(
   className: "photo-show-container",
 
   events: {
-    "click .close-modal": "close"
+    "click .close-modal": "close",
+
   },
 
   render: function () {
     var content = this.template({ photo: this.model });
     this.$el.html(this.makeModal({ content: content }));
+    $(window).on('resize', this.setStyle.bind(this));
+    this.setStyle();
     return this;
+  },
+
+  setStyle: function () {
+    var $img = this.$el.find('img');
+    var width = window.innerWidth * 0.7;
+    var height = window.innerHeight * 0.7;
+    $img.css('max-width', width);
+    $img.css('max-height', height);
   },
 
   close: function () {
     this.remove();
+  },
+
+  remove: function () {
+    $(window).off("resize", this.setStyle);
+    Backbone.View.prototype.remove.apply(this, arguments);
   }
 }));
