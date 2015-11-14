b9503b048491fdec7bce474cc7028971535bee42
Jordan Roth <jmtroth0@gmail.com>
2015-11-14 02:28:48 -0500

    Fix ordering bug

--- app/assets/javascripts/collections/family_members.js
+++ app/assets/javascripts/collections/family_members.js
@@ -2,10 +2,14 @@ Genealogy.Collections.FamilyMembers = Backbone.Collection.extend({
   model: Genealogy.Models.FamilyMember,
   url: 'api/family_members',
 
-// when adding age to attributes, add that facet 
+// when adding age to attributes, add that facet
   comparator: function (person, otherPerson) {
     var origin = person.escape('generation');
     var other = otherPerson.escape('generation');
+
+    if (origin) origin = parseInt(origin);
+    if (other) other = parseInt(other);
+
     if (origin !== origin || origin > other) {
       return -1;
     } else if (origin === other) {
