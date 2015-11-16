f9b4406686db29590ef88c552d55768c82c34eea
Jordan Roth <jmtroth0@gmail.com>
2015-11-15 19:42:40 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113211102) do
+ActiveRecord::Schema.define(version: 20151115175359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,19 @@ ActiveRecord::Schema.define(version: 20151113211102) do
 
   add_index "photos", ["uploader_id"], name: "index_photos_on_uploader_id", using: :btree
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "family_member_id", null: false
+    t.string   "taggable_type"
+    t.integer  "taggable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "taggings", ["family_member_id"], name: "index_taggings_on_family_member_id", using: :btree
+  add_index "taggings", ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id", using: :btree
+
   create_table "users", force: :cascade do |t|
-    t.string   "username",                           null: false
+    t.string   "email",                              null: false
     t.string   "password_digest",                    null: false
     t.string   "session_token",                      null: false
     t.datetime "created_at",                         null: false
@@ -66,7 +77,7 @@ ActiveRecord::Schema.define(version: 20151113211102) do
     t.string   "lname",           default: "Schmoe"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
