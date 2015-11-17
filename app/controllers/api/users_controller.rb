102e633230fe27d59442bfc7da17fb2fb01fa088
Jordan Roth <jmtroth0@gmail.com>
2015-11-17 03:29:53 -0500

    Distinguish between teacher and student in controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,13 @@ module Api
   class UsersController < ApiController
     def current
       @user = User.find_by(session_token: session[:token])
-      render :show
+      if @user.instance_of? Teacher
+        @sections = @user.sections
+        render 'api/teachers/show'
+      elsif @user.instance_of? Student
+        @section = @user.section
+        render 'api/students/show'
+      end
     end
   end
 end
