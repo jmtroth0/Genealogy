824317677458353320f3b4c89709586b2f5e9433
Jordan Roth <jmtroth0@gmail.com>
2015-11-22 20:01:15 -0500

    Move controller into api module

---
+++ app/controllers/api/submissions_controller.rb
@@ -0,0 +1,46 @@
+module Api
+  class SubmissionsController < ApiController
+    def index
+      @submissions = Submission.includes(:uploads).find_by({
+        unit_id: params[:unit_id],
+        user_id: current_user.id
+      })
+    end
+
+    def show
+      @submission = Submission.includes(:uploads).find(params[:id])
+    end
+
+    def create  # handle file uploads in a transaction soon too
+      @submission = current_user.submissions.new(submission_params)
+      @submission.unit_id = params[:unit_id]
+
+      if @submission.save
+        render :show
+      else
+        render json: @submission.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @submission = Submission.find(params[:id])
+
+      if @submission.update(submission_params)
+        render :show
+      else
+        render json: @submission.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @submission = Submission.find(params[:id])
+      @submission.destroy!
+      render :show
+    end
+
+    private
+    def submission_params
+      params.require(:submission).permit(:title, :description, upload_ids: [])
+    end
+  end
+end
