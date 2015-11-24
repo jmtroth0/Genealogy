134aff59e1d9fb4192da376636caa6be8f410731
Jordan Roth <jmtroth0@gmail.com>
2015-11-23 23:11:11 -0500

    Add resize capability to pdf files

--- app/assets/javascripts/views/documents/index.js
+++ app/assets/javascripts/views/documents/index.js
@@ -9,4 +9,31 @@ Genealogy.Views.DocumentsIndex = Genealogy.Views.IndexView.extend(
 
     Genealogy.Views.IndexView.prototype.initialize.call(this);
   },
+
+  render: function () {
+    Genealogy.Views.IndexView.prototype.render.call(this);
+    setTimeout(function () {
+      this.addDragResize();
+    }.bind(this), 1000);
+  },
+
+  addDragResize: function () {
+    interact('li.document-member')
+      .draggable({ onmove: window.dragMoveListener })
+      .resizable({ edges: { bottom: true } })
+      .on('resizemove', function (event) {
+        var target = event.target,
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+        // update the element's style
+        target.style.height = event.rect.height + 'px';
+
+        y += event.deltaRect.top;
+
+        target.style.webkitTransform = target.style.transform =
+            'translate(' + y + 'px)';
+
+        target.setAttribute('data-y', y);
+      });
+  },
 }));
